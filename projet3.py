12fefcc42fae1a66723cbb790959410fb518bc7e
lalouve <florence.dervaux@gmail.com>
2014-11-21 00:04:37 +0100

    Premier commit fonction d'affichage de la grille de jeu

---
+++ projet3.py
@@ -0,0 +1,54 @@
+'''
+Dervaux Florence, N° de matricule : 000396246, groupe 1
+
+projet 3: Le démineur
+'''
+
+#constantes
+widht = 9
+
+#grille par défaut 
+grille = {}
+grille[(0, 7)] = 'B'
+grille[(1, 5)] = 'B'
+grille[(1, 6)] = 'B'
+grille[(1, 8)] = 'B'
+grille[(2, 4)] = 'B'
+grille[(3, 4)] = 'B'
+grille[(5, 5)] = 'B'
+grille[(5, 7)] = 'B'
+grille[(7, 0)] = 'B'
+grille[(7, 5)] = 'B'
+
+matrice = [['*','*','*','*','*','*','*','*','*'],
+           ['*','*','*','*','*','*','*','*','*'],
+           ['*','*','*','*','*','*','*','*','*'],
+           ['*','*','*','*','*','*','*','*','*'],
+           ['*','*','*','*','*','*','*','*','*'],
+           ['*','*','*','*','*','*','*','*','*'],
+           ['*','*','*','*','*','*','*','*','*'],
+           ['*','*','*','*','*','*','*','*','*'],
+           ['*','*','*','*','*','*','*','*','*']]
+
+
+
+def showGrille(matrice):
+    print('  ', end=' ')
+    for i in range(1, 10, 1):
+        print(i, end='   ')
+    print()
+    print(' ', 36*'-')   
+
+    letter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+    count = 0
+    for j in matrice:
+        print(letter[count], end=' |')
+        for k in j:
+            print(k, end=' | ')
+        print('')
+        print(' ', 36 * '-')
+        count += 1
+
+showGrille(matrice)
+
+

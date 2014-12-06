1819934a510189ea75bd137c5348dae23c487415
lalouve <florence.dervaux@gmail.com>
2014-12-06 17:34:37 +0100

    fonction décodage

--- projet4.py
+++ projet4.py
@@ -2,7 +2,10 @@
 Dervaux Florence, N° de matricule : 000396246, groupe 4
 
 Projet 4: Introdiction à la sténographie
+
+The cake is a lie! 
 '''
+from sys import argv
 
 def info_image(nom_image):
     '''
@@ -17,14 +20,17 @@ def info_image(nom_image):
         width, height, valMax = None, None, None
     else:
         size = (image.readline()).split()
-        width = size[0]
-        height = size[1]
-        valMax = (image.readline()).strip()
+        width = int(size[0])
+        height = int(size[1])
+        valMax = int((image.readline()).strip())
 
     image.close()
-    return (identifier, int(width), int(height), int(valMax))
+    return (identifier, width, height, valMax)
 
 def charger_image(nom_image):
+    '''
+    lit le fichier image et renvoie la matrice des pixels
+    '''
     identifier, width, height, valMax = info_image(nom_image)
     
     pixelMatrix = []
@@ -46,7 +52,21 @@ def charger_image(nom_image):
     
     return pixelMatrix
 
+def writePGM(nom_image, pgm, info):
+    '''
+    Permet d'écrire les infos dans un fichier pgm
+    '''
+    fichier = open(nom_image,"w")
+    body = "\n".join([" ".join([str(y) for y in x]) for x in pgm])
+
+
+    fichier.write("P2\n{0} {1}\n{2}\n{3}\n".format(info[1], info[2], info[3],body))
+    fichier.close()
+
 def encodageMethode(source, code):
+    '''
+    crée le corps du fichier a encoder
+    '''
     infoSource = info_image(source)
     bodySource = charger_image(source)
 
@@ -63,15 +83,23 @@ def encodageMethode(source, code):
 
     return bodySource
 
-def writePGM(nom_image, pgm, info):
-    fichier = open(nom_image,"w")
-    body = "\n".join([" ".join([str(y) for y in x]) for x in pgm])
-
-
-    fichier.write("P2\n{0} {1}\n{2}\n{3}\n".format(info[1], info[2], info[3],body))
-    fichier.close()
+def decodageMethode(crypt):
+    '''
+    crée le corps du fichier décrypté
+    '''
+    infoCrypt = info_image(crypt)
+    bodyCrypt = charger_image(crypt)
+    
+    for i in range(infoCrypt[2]):
+        for j in range(infoCrypt[1]):
+            bodyCrypt[i][j] %= 2
+    return bodyCrypt
 
 def encodage(source, code, res):
+    '''
+    permet l'encodage d'une image "code" dans une image "source"
+    le résultat est inscrit dans une troisième image "res"
+    '''
     infoSource = None
     infoCode = None
 
@@ -85,20 +113,55 @@ def encodage(source, code, res):
         if infoCode[3] != 1:
             print("Le fichier {0} n'est pas au bon format !".format(code))
             infoCode = None
+        
+        if infoCode[0] != 'P2':
+            print ("Le fichier {0} n'est pas au bon format !".format(code))
+            infoCode = None
+    
     except IOError as e:
         print("Impossible d'ouvrir {0}".format(code))
 
     if not(infoSource is None or infoCode is None): 
         writePGM(res, encodageMethode(source, code), infoSource)
 
+def decodage(crypt, claire):
+    '''
+    Permet le décodage d'une image cryptée, "crypt"
+    le résultat est inscrit dans une nouvelle image "claire"
+    '''
+    infoCrypt = None
+    try:
+        infoCrypt = info_image(crypt)
+        infoCrypt = (infoCrypt[0], infoCrypt[1], infoCrypt[2], 1)
+        
+        if infoCrypt[0] != 'P2':
+            print ("Le fichier {0} n'est pas au bon format !".format(crypt))
+            infoCrypt = None
+    
+    except IOError as e:
+        print("Impossible d'ouvrir {0}".format(crypt))
+
+
+    if not(infoCrypt is None):
+        writePGM(claire, decodageMethode(crypt), infoCrypt)
+
 
 
 if __name__=='__main__':
-  if len(argv) == 5 and argv[1] == "codage":
-     encodeProcedure(argv[2], argv[3], argv[4]) 
-  elif len(argv) == 4 and argv[1] == "decodage":
-     decodeProcedure(argv[2], argv[3])
-  elif argv[1] not in ["codage", "decodage"]:
-     print("Operation non comprise.") 
-  else:
-     print("nombre de paramètre non conforme.")
+    print("\nProjet sténographie:\n", 18*'~')
+    if len(argv) == 5 and argv[1] == "codage":
+        text = "Fichier source: {}\nFichier code: {}\nFichier destination: {}".format(argv[2], argv[3], argv[4])
+        print(text)
+        encodage(argv[2], argv[3], argv[4]) 
+        print("Codage: Travail terminé !")
+    
+    elif len(argv) == 4 and argv[1] == "decodage":
+        text = "Fichier source: {}\nFichier destination: {}".format(argv[2], argv[3])
+        print(text)
+        decodage(argv[2], argv[3])
+        print("Décodage: Travail terminé !")
+
+    elif argv[1] not in ["codage", "decodage"]:
+        print("Operation non comprise.") 
+    else:
+        print("nombre de paramètre non conforme.")

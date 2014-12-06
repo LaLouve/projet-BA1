b4bfea192d2369d61c8e42ce91f142c78f24464a
lalouve <florence.dervaux@gmail.com>
2014-12-06 16:42:13 +0100

    Codage fonctionnel appelation du programme avec arguments

--- projet4.py
+++ projet4.py
@@ -22,36 +22,83 @@ def info_image(nom_image):
         valMax = (image.readline()).strip()
 
     image.close()
-    return (identifier, width, height, valMax)
+    return (identifier, int(width), int(height), int(valMax))
 
 def charger_image(nom_image):
     identifier, width, height, valMax = info_image(nom_image)
-    sizeX = int(width)
-    sizeY = int(height)
-
-    image = open(nom_image)
-    imageBrut = image.read().split()[1:]
-
+    
     pixelMatrix = []
-    for y in range(sizeY):
-        lineOffset = sizeX*y
-        line = []
-        for x in range(sizeX):
-           line.append(int(imageBrut[lineOffset + x + 3 ]))
-        pixelMatrix.append(line)
+
+    if identifier == 'P2':
+        sizeX = int(width)
+        sizeY = int(height)
+
+        image = open(nom_image)
+        imageBrut = image.read().split()[1:]
+
+        for y in range(sizeY):
+            lineOffset = sizeX*y
+            line = []
+            for x in range(sizeX):
+               line.append(int(imageBrut[lineOffset + x + 3 ]))
+            pixelMatrix.append(line)
+        image.close()
     
     return pixelMatrix
 
+def encodageMethode(source, code):
+    infoSource = info_image(source)
+    bodySource = charger_image(source)
+
+    infoCode = info_image(code)
+    bodyCode = charger_image(code)
+
+
+    for i in range(infoSource[2]):
+        for j in range(infoSource[1]):
+            parite = bodySource[i][j] % 2
+            if parite == 0 and bodyCode[i][j] != 0 and bodySource[i][j] == infoSource[3]:
+                bodySource[i][j] -= 2
+            bodySource[i][j] += bodyCode[i][j] - parite
+
+    return bodySource
+
+def writePGM(nom_image, pgm, info):
+    fichier = open(nom_image,"w")
+    body = "\n".join([" ".join([str(y) for y in x]) for x in pgm])
+
+
+    fichier.write("P2\n{0} {1}\n{2}\n{3}\n".format(info[1], info[2], info[3],body))
+    fichier.close()
+
+def encodage(source, code, res):
+    infoSource = None
+    infoCode = None
+
+    try:
+        infoSource = info_image(source)
+    except IOError as e:
+        print("Impossible d'ouvrir {0}".format(source))
+
+    try:
+        infoCode = info_image(code)
+        if infoCode[3] != 1:
+            print("Le fichier {0} n'est pas au bon format !".format(code))
+            infoCode = None
+    except IOError as e:
+        print("Impossible d'ouvrir {0}".format(code))
+
+    if not(infoSource is None or infoCode is None): 
+        writePGM(res, encodageMethode(source, code), infoSource)
 
 
 
-
-
-
-
-def main(): 
-    print(charger_image('math.pgm'))
-
-
-if __name__ == "__main__":
-    main()
+if __name__=='__main__':
+  if len(argv) == 5 and argv[1] == "codage":
+     encodeProcedure(argv[2], argv[3], argv[4]) 
+  elif len(argv) == 4 and argv[1] == "decodage":
+     decodeProcedure(argv[2], argv[3])
+  elif argv[1] not in ["codage", "decodage"]:
+     print("Operation non comprise.") 
+  else:
+     print("nombre de paramètre non conforme.")
